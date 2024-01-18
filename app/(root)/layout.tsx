17f5f765cbe7b82edcaed9258e9ceb190cfb0192
Gavin Garza <gaving@live.com>
2024-01-18 23:59:50 +0900

    asdad

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "../globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
+impo
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,9 +20,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <TopBar/>
-          
+          <TopBar />
+          <main>
+            <LeftSidebar
+            <RightSidebar />
+          </main>
           {children}
+          <BottomBar />
         </body>
       </html>
     </ClerkProvider>
