8f457c749832e8dc59d85c1497d4c946fc9d5724
Gavin Garza <gaving@live.com>
2024-05-03 15:45:34 +0900

    action for fetching indivdual thread created

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -65,10 +65,20 @@ const ThreadCard = ({
             <div className="mt-5 flex flex-col gap-3">
               <div className='flex gap-3.5'>
                 <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
-                <Image src="/assets/reply.svg" alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
+                <Link href={`/thread/${id}`}>
+                  <Image src="/assets/reply.svg" alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
+                </Link>
                 <Image src="/assets/repost.svg" alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
                 <Image src="/assets/share.svg" alt="heart" width={24} height={24} className='cursor-pointer object-contain' />
               </div>
+
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className='mt-1 text-subtle-medium text-gray-1 cursor-pointer'>
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -3,8 +3,7 @@
 import { revalidatePath } from "next/cache"
 import Thread from "../models/thread.model"
 import User from "../models/user.model"
-import { connectToDB } from "../mongoose"
-import { connect } from "http2"
+import { connectToDB } from "../mongoose" 
 
 interface Params {
   text: string,
@@ -40,18 +39,18 @@ export async function fetchThreads(pageNumber = 1, pageSize = 20) {
     const skipAmount = (pageNumber - 1) * pageSize
 
     const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] }})
-    .sort({ createdAt: 'desc' })
-    .skip(skipAmount)
-    .limit(pageSize)
-    .populate({path: 'author', model: User})
-    .populate({
-      path: 'children',
-      populate: {
-        path: 'author',
-        model: User,
-        select: "_id name parentId image"
-      }
-    })
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({path: 'author', model: User})
+      .populate({
+        path: 'children',
+        populate: {
+          path: 'author',
+          model: User,
+          select: "_id name parentId image"
+        }
+      })
 
     const totalThreadsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] }})
 
@@ -63,5 +62,39 @@ export async function fetchThreads(pageNumber = 1, pageSize = 20) {
 
   } catch(error: any) {
     throw new Error(`Failed to fetch threads: ${error.message}`)
+  }
+}
+
+export async function fetchThreadById(id: string) {
+  try {
+    connectToDB()
+
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: "_id id name image"
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: "_id id name parentId image"
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: "_id id name parentId image"
+            }
+          }
+        ]
+      })
+  } catch(error: any) {
+    throw new Error(`Failed to fetch thread: ${error.message}`)
   }
 }
